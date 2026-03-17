903c0e4b5c6449add7d8b332ce2b459c891c3ad1
Gabriel Tseng <gabriel.tseng@dauphine.eu>
2026-03-17 14:55:20 +0100

    New strategies : Random and Proba Coop

--- strategies.py
+++ strategies.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-
+import random
+import default_params
 
 class Strategy(ABC) :
     """ Common interface for predefined strategies. (Like interface in Java)
@@ -12,8 +13,25 @@ class Strategy(ABC) :
 
 class AlwaysCooperate(Strategy) :
     def choose_action(self, my_id, other_player_id, interactions) -> str:
+        """No matter what, cooperate.
+        """
         return "C"
 
 class AlwaysBetray(Strategy):
     def choose_action(self, my_id, other_player_id, interactions) -> str:
+        """No matter what, betrays.
+        """
         return "B"
+
+class RandomAction(Strategy):
+    def choose_action(self, my_id, other_player_id, interactions) -> str:
+        """Takes a random choice at each interaction.
+        """
+        return random.choice(["C", "B"])
+
+class ProbaCooperation(Strategy):
+    def choose_action(self, my_id, other_player_id, interactions) -> str:
+        """Cooperate with a fixed probability given in the default params.
+        """
+        return random.choices(["C", "D"], weights=[default_params.P_COOP, 1-default_params.P_COOP])[0]
+
